f48a62f8dad91b2bbe8666aafdd86f3a75a288ab
Jim Nutt <jim@nuttz.org>
2025-09-30 15:01:34 -0700

    Added more error handling and other features
    automatic initiative for mobs
    save and load history file
    better error handling

--- gmshell.py
+++ gmshell.py
@@ -2,6 +2,7 @@
 import cmd, sys
 import csv
 import pickle
+import readline
 
 from io import StringIO
 from player import Player
@@ -20,176 +21,226 @@ class GMShell(cmd.Cmd):
 		return True
 
 	def findPlayer(self, pn):
-		for p in self.players:
-			if p.getNick() == pn or p.getName() == pn:
-				return p
+		'Find player by name'
+
+		try:
+			for p in self.players:
+				if p.getNick() == pn or p.getName() == pn:
+					return p
+		except:
+			print("error locating player {}".format(pn))
 		return None
 
 
 	def do_player(self, arg):
 		'Set player information'
 
-		p = parse(arg.replace("'",'"'))
+		try:
+			p = parse(arg.replace("'",'"'))
 
-		pn = p.pop(0).lower();
-		c = "unknown"
-		l = 0
-		a = 0
-		per = 0
-		inv = 0
-		ins = 0
-		init = 0
-		nick = None
-
-		player = self.findPlayer(pn)
-		for i in p:
-			n,v = i.split('=')
-			n = n.lower()
-			if (n.startswith('c')):
-				c = v
-				if player is not None:
-					  player.setClass(c)
-			elif (n.startswith('l')):
-				l = int(v)
-				if player is not None:
-					player.setLevel(l)
-			elif (n.startswith('a')):
-				a = int(v)
-				if player is not None:
-					player.setAC(a)
-			elif (n.startswith('ini')):
-				init = int(v)
-				if player is not None:
-					player.setInitiative(init)
-			elif (n.startswith('per')):
-				per = int(v)
-				if player is not None:
-					player.setPerception(per)
-			elif (n.startswith('inv')):
-				inv = int(v)
-				if player is not None:
-					player.setInvestigation(inv)
-			elif (n.startswith('ins')):
-				ins = int(v)
-				if player is not None:
-					player.setInsight(ins)
-			elif (n.startswith('nick')):
-				nick = v
-				if player is not None:
-					player.setNick(nick)
-
-		if player is None:
-			player = Player(pn, pclass=c, level=l, ac=a, per=per, inv=inv, ins=ins, init=init, nick=nick)
-			self.players.append(player)
-			print("Player \'{}\' added".format(pn))
-		else:
-			print("Player \'{}\' updated".format(pn))
-
-	def do_mob(self, arg):
-		'Set mob information'
-		p = parse(arg)
-
-		mn = p.pop(0).lower();
-		if p[0] == "delete" and mn in mobs:
-			del self.mobs[mn]
-			print("Mob \'{}\' removed".format(mn))
-		else:
+			pn = p.pop(0).lower();
+			c = "unknown"
+			l = 0
 			a = 0
-			h = 0
+			per = 0
+			inv = 0
+			ins = 0
 			init = 0
-			exists = mn in self.mobs
+			nick = None
+
+			player = self.findPlayer(pn)
 			for i in p:
 				n,v = i.split('=')
 				n = n.lower()
-				if (n.startswith('a')):
+				if (n.startswith('c')):
+					c = v
+					if player is not None:
+						  player.setClass(c)
+				elif (n.startswith('l')):
+					l = int(v)
+					if player is not None:
+						player.setLevel(l)
+				elif (n.startswith('a')):
 					a = int(v)
-					if exists:
-						self.mobs[mn].setAC(a)
-				elif (n.startswith('i')):
+					if player is not None:
+						player.setAC(a)
+				elif (n.startswith('ini')):
 					init = int(v)
+					if player is not None:
+						player.setInitiative(init)
+				elif (n.startswith('per')):
+					per = int(v)
+					if player is not None:
+						player.setPerception(per)
+				elif (n.startswith('inv')):
+					inv = int(v)
+					if player is not None:
+						player.setInvestigation(inv)
+				elif (n.startswith('ins')):
+					ins = int(v)
+					if player is not None:
+						player.setInsight(ins)
+				elif (n.startswith('nick')):
+					nick = v
+					if player is not None:
+						player.setNick(nick)
 
-					if exists:
-						self.mobs[mn].setInitiative(init)
-				elif (n.startswith('h')):
-					h = int(v)
-					if exists:
-						self.mobs[mn].setHP(h)
-				elif (n.startswith('-h')):
-					h = int(v)
-					if exists:
-						self.mobs[mn].setHP(self.mobs[mn].getHP() - h)
-				elif (n.startswith('+h')):
-					h = int(v)
-					if exists:
-						self.mobs[mn].setHP(self.mobs[mn].getHP() + h)
+			if player is None:
+				player = Player(pn, pclass=c, level=l, ac=a, per=per, inv=inv, ins=ins, init=init, nick=nick)
+				self.players.append(player)
+				print("Player \'{}\' added".format(pn))
+			else:
+				print("Player \'{}\' updated".format(pn))
+			player.print(sys.stdout)
+		except Exception as ex:
+			print("Got exception {} trying to process {}".format(ex,arg))
 
-			if not exists:
-				mob = Mob(mn, ac=a, hp=h, init=init)
-				self.mobs[mob.getName()] = mob
-			print("Mob \'{}\' {}".format(mn, 'updated' if exists else 'added'))
+	def do_mob(self, arg):
+		'Set mob information'
+
+		try: 
+			p = parse(arg)
+
+			mn = p.pop(0).lower();
+			if p[0] == "delete" and mn in self.mobs:
+				del self.mobs[mn]
+				print("Mob \'{}\' removed".format(mn))
+			else:
+				a = 0
+				h = 0
+				init = 0
+				exists = mn in self.mobs
+				for i in p:
+					try:
+						n,v = i.split('=')
+						n = n.lower()
+						if (n.startswith('a')):
+							a = int(v)
+							if exists:
+								self.mobs[mn].setAC(a)
+						elif (n.startswith('i')):
+							init = int(v)
+
+							if exists:
+								self.mobs[mn].setInitiative(init)
+						elif (n.startswith('h')):
+							h = int(v)
+							if exists:
+								self.mobs[mn].setHP(h)
+						elif (n.startswith('b')):
+							b = int(v)
+							if exists:
+								self.mobs[mn].setBonus(b)
+						elif (n.startswith('-h')):
+							h = int(v)
+							if exists:
+								self.mobs[mn].setHP(self.mobs[mn].getHP() - h)
+							if self.mobs[mn].getHP() < 1:
+								print("{} is dead".format(mn))
+								del self.mobs[mn]
+						elif (n.startswith('+h')):
+							h = int(v)
+							if exists:
+								self.mobs[mn].setHP(self.mobs[mn].getHP() + h)
+					except ValueError:
+						print("Missing or bad value in {}".format(i))
+
+				if not exists:
+					mob = Mob(mn, ac=a, hp=h, init=init)
+					self.mobs[mob.getName()] = mob
+				print("Mob \'{}\' {}".format(mn, 'updated' if exists else 'added'))
+				if mn in self.mobs:
+					self.mobs[mn].print(sys.stdout)
+		except Exception as ex:
+			print("Got exception {} trying to process {}".format(ex,arg))
 
 	def do_players(self, arg):
-		plist = sorted(self.players,reverse=True)
-		if arg.startswith("save"):
-			c,fn = arg.split(" ")
-			print("saving players to {}".format(fn))
-			self.save(self.players,fn)
-		elif arg.startswith("load"):
-			c,fn = arg.split(" ")
-			print("loading players from {}".format(fn))
-			self.players = self.load(fn)
-			self.do_players("list")
-		else:
-			for p in plist:
-				p.print(sys.stdout)
-				print()
+		try:
+			plist = sorted(self.players,reverse=True)
+			if arg.startswith("save"):
+				c,fn = arg.split(" ")
+				print("saving players to {}".format(fn))
+				self.save(self.players,fn)
+			elif arg.startswith("load"):
+				c,fn = arg.split(" ")
+				print("loading players from {}".format(fn))
+				pl = self.load(fn)
+				if pl is not None:
+					for p in pl:
+						if self.findPlayer(p.getName()) is None:
+							self.players.append(p)
+				self.do_players("list")
+			else:
+				for p in plist:
+					p.print(sys.stdout)
+					print()
+		except Exception as ex:
+			print("Got exception {} trying to process {}".format(ex,arg))
 
 	def save(self, obj, fn):
-		with open(fn,"wb") as f:
-			pickle.dump(obj,f)
+		try:
+			with open(fn,"wb") as f:
+				pickle.dump(obj,f)
+		except:
+			print("Did not save file {}".format(fn))
 
 	def load(self, fn):
-		with open(fn,"rb") as f:
-			return pickle.load(f)
+		try:
+			with open(fn,"rb") as f:
+				return pickle.load(f)
+		except:
+			print("Did not load file {}".format(fn))
+			return None
 
 	def do_mobs(self, arg):
-		mlist = sorted(self.mobs.values(),reverse=True)
-		if arg.startswith("save"):
-			c,f = arg.split(" ")
-			print("saving mobs to {}".format(f))
-			self.save(self.mobs,f)
-		elif arg.startswith("load"):
-			c,f = arg.split(" ")
-			print("loading mobs from {}".format(f))
-			self.mobs = self.load(f)
-			self.do_mobs("list")
-		elif arg.startswith("cl"):
-			self.mobs = dict()
-		else:
-			for m in mlist:
-				m.print(sys.stdout)
-				print()
+		try:
+			mlist = sorted(self.mobs.values(),reverse=True)
+			if arg.startswith("save"):
+				c,f = arg.split(" ")
+				print("saving mobs to {}".format(f))
+				self.save(self.mobs,f)
+			elif arg.startswith("load"):
+				c,f = arg.split(" ")
+				print("loading mobs from {}".format(f))
+				m = self.load(f)
+				if m is not None:
+					self.mobs.update(m)
+				self.do_mobs("list")
+			elif arg.startswith("cl"):
+				self.mobs = dict()
+			elif arg.startswith('ro'):
+				for m in mlist:
+					m.rollInit()
+			else:
+				for m in mlist:
+					m.print(sys.stdout)
+					print()
+		except Exception as ex:
+			print("Got exception {} trying to process {}".format(ex,arg))
 
 	def initiative(self, arg):
-		plist = sorted(self.players,reverse=True)
-		mlist = sorted(self.mobs.values(),reverse=True)
-		combatants = list(plist)
-		combatants.extend(mlist)
-		initorder = sorted(combatants, reverse=True)
-		self.nextp = self.nextp % len(initorder)
+		try:
+			plist = sorted(self.players,reverse=True)
+			mlist = sorted(self.mobs.values(),reverse=True)
+			combatants = list(plist)
+			combatants.extend(mlist)
+			initorder = sorted(combatants, reverse=True)
+			self.nextp = self.nextp % len(initorder)
 
-		if arg == "list" or len(arg) == 0:
-			for c in initorder[self.nextp:]:
-				print("{} {}".format(c.getName(),c.getInitiative()))
-			for c in initorder[:self.nextp]:
-				print("{} {}".format(c.getName(),c.getInitiative()))
-		
-		elif arg == "clear" or arg == 'reset':
-			self.nextp = 0
+			if arg == "list" or len(arg) == 0:
+				for c in initorder[self.nextp:]:
+					print("{} {}".format(c.getName(),c.getInitiative()))
+				for c in initorder[:self.nextp]:
+					print("{} {}".format(c.getName(),c.getInitiative()))
+			
+			elif arg == "clear" or arg == 'reset':
+				self.nextp = 0
 
-		elif arg == "next":
-			print(initorder[self.nextp].getName())
-			self.nextp += 1
+			elif arg == "next":
+				print(initorder[self.nextp].getName())
+				self.nextp += 1
+		except Exception as ex:
+			print("Got exception {} trying to process {}".format(ex,arg))
 
 	def do_initiative(self,arg):
 		self.initiative(arg)
@@ -206,5 +257,11 @@ def parse(arg):
     return reader.__next__()
 
 if __name__ == '__main__':
-    GMShell().cmdloop()
+	try:
+		readline.read_history_file('.gmshell.history')
+	except:
+		with open('.gmshell.history','w'):
+			print("no history to load")
+	GMShell().cmdloop()
+	readline.write_history_file('.gmshell.history')
 
--- mob.py
+++ mob.py
@@ -1,17 +1,31 @@
 import combatant
+import secrets
 
 class Mob(combatant.Combatant):
-	def __init__(self, n, hp=0, ac=0, init=0):
+	def __init__(self, n, hp=0, ac=0, init=0, bonus=0):
 		self.hp = hp
 		self.n = n
 		self.ac = ac
 		self.init = init
+		self.bonus = bonus
 
 	def getHP(self):
 		return self.hp
 
 	def setHP(self, arg):
 		self.hp = arg
+
+	def getBonus(self):
+		return self.bonus
+
+	def setBonus(self, arg):
+		try: 
+			self.bonus = int(arg)
+		except:
+			self.bonus=0
+
+	def rollInit(self):
+		self.init = secrets.choice(range(20)) + 1 + self.bonus
 
 	def print(self, out):
 		print("Name: {}".format(self.n),file=out)
